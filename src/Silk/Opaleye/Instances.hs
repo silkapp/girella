685bda2f752867849bc3e42f6e4566e949a45567
Adam Bergmark <adam@bergmark.nl>
2014-12-02 00:27:17 +0100

    Remove orphans that have moved into opaleye

--- src/Silk/Opaleye/Instances.hs
+++ src/Silk/Opaleye/Instances.hs
@@ -9,36 +9,11 @@
 module Silk.Opaleye.Instances where
 
 import Data.CaseInsensitive (CI)
-import Data.Time
-import System.Locale
 import Data.Profunctor.Product.Default
-import Data.UUID (UUID)
 import Database.PostgreSQL.Simple.FromField (FromField (..))
 import Opaleye.Internal.HaskellDB.Query (ShowConstant (..))
 import Opaleye.RunQuery
 import qualified Data.CaseInsensitive as CI
-import qualified Data.Text            as ST
-import qualified Data.Text.Lazy       as LT
-import qualified Data.UUID            as UUID
-
-instance ShowConstant UUID where
-  showConstant = showConstant . UUID.toString
-
-instance ShowConstant ST.Text where
-  showConstant = showConstant . ST.unpack
-
-instance ShowConstant LT.Text where
-  showConstant = showConstant . LT.unpack
-
-instance ShowConstant UTCTime where
-  showConstant = showConstant . formatTime defaultTimeLocale format
-    where
-      format = "%Y-%m-%dT%H:%M:%SZ"
-
-instance Default QueryRunnerColumn UUID    UUID    where def = fieldQueryRunnerColumn
-instance Default QueryRunnerColumn ST.Text ST.Text where def = fieldQueryRunnerColumn
-instance Default QueryRunnerColumn LT.Text LT.Text where def = fieldQueryRunnerColumn
-instance Default QueryRunnerColumn UTCTime UTCTime where def = fieldQueryRunnerColumn
 
 instance FromField (CI a) => Default QueryRunnerColumn (CI a) (CI a) where
   def = fieldQueryRunnerColumn
