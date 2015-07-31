4d493d1fd9cfe402a8b201d4e03cedbf85a6cb5d
Adam Bergmark <adam@bergmark.nl>
2015-07-31 17:16:52 +0200

    Proxy all aggregations except for arrayAgg

--- src/Silk/Opaleye/Aggregation.hs
+++ src/Silk/Opaleye/Aggregation.hs
@@ -1,38 +1,58 @@
 {-# LANGUAGE
     FlexibleContexts
+  , NoMonomorphismRestriction
   , TypeFamilies
   #-}
 module Silk.Opaleye.Aggregation
-  ( Aggregator
-  , aggregate
-  , boolAnd
-  , boolOr
+  ( sum_
   , count
+  , avg
+  , max_
+  , min_
+  , boolOr
+  , boolAnd
+  , stringAgg
+  -- * Re-exports
+  , Aggregator
+  , aggregate
   , groupBy
-  , maxAggr
-  , minAggr
   ) where
 
+import Data.Int (Int64)
 import Data.Profunctor (Profunctor, dimap)
 
-import Opaleye.Aggregate (Aggregator, aggregate, count, groupBy)
+import Opaleye.Aggregate (Aggregator, aggregate, groupBy)
 import Opaleye.Column (Column)
-import Opaleye.PGTypes (PGBool)
-import qualified Opaleye.Aggregate as A (boolAnd, boolOr, max, min)
+import Opaleye.PGTypes (PGBool, PGText)
+import qualified Opaleye.Aggregate as A (avg, boolAnd, boolOr, count, max, min, stringAgg, sum)
 
 import Silk.Opaleye.ShowConstant
 
-boolOr :: (ShowConstant a, PGRep a ~ PGBool) => Aggregator (Column a) (Column a)
+sum_ :: Aggregator (Column a) (Column a)
+sum_ = safeAgg A.sum
+
+count :: Aggregator (Column a) (Column Int64)
+count = dimap id safeCoerceFromRep A.count
+
+avg :: Aggregator (Column Double) (Column Double)
+avg = safeAgg A.avg
+
+max_ :: PGOrd (PGRep a) => Aggregator (Column a) (Column a)
+max_ = safeAgg A.max
+
+min_ :: PGOrd (PGRep a) => Aggregator (Column a) (Column a)
+min_ = safeAgg A.min
+
+boolOr :: PGRep a ~ PGBool => Aggregator (Column a) (Column a)
 boolOr = safeAgg A.boolOr
 
-boolAnd :: (ShowConstant a, PGRep a ~ PGBool) => Aggregator (Column a) (Column a)
+boolAnd :: PGRep a ~ PGBool => Aggregator (Column a) (Column a)
 boolAnd = safeAgg A.boolAnd
 
-maxAggr :: (ShowConstant a, PGOrd (PGRep a)) => Aggregator (Column a) (Column a)
-maxAggr = safeAgg A.max
+stringAgg :: PGRep a ~ PGText => Column a -> Aggregator (Column a) (Column a)
+stringAgg = safeAgg . A.stringAgg . safeCoerceToRep
 
-minAggr :: (ShowConstant a, PGOrd (PGRep a)) => Aggregator (Column a) (Column a)
-minAggr = safeAgg A.min
+-- TODO: Missing arrayAgg
 
 safeAgg :: Profunctor p
   => p (Column (PGRep a)) (Column (PGRep b))
