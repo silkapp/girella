7de996f73ded4797c8d5cbe360e8a509d62d1281
Adam Bergmark <adam@bergmark.nl>
2015-09-08 03:51:14 +0200

    Add Conv instances for tuples up to 8 elements, and add identity Conv instances for all built-in types, and Conv a => Conv (CI a) (closes #5, closes #6)

--- src/Silk/Opaleye/Conv.hs
+++ src/Silk/Opaleye/Conv.hs
@@ -1,8 +1,16 @@
 {-# LANGUAGE
     DefaultSignatures
+  , FlexibleInstances
   , TypeFamilies
+  , TypeSynonymInstances
   #-}
 module Silk.Opaleye.Conv (Conv (..)) where
+
+import Data.CaseInsensitive
+import Data.Int
+import Data.String.Conversions
+import Data.Time
+import Data.UUID
 
 -- | Convert an opaleye result to a separate type.
 --
@@ -16,6 +24,61 @@ class Conv d where
   default conv :: (OpaRep d ~ d) => OpaRep d -> d
   conv = id
 
+instance Conv Char
+instance Conv (Maybe Char)
+instance Conv StrictText
+instance Conv (Maybe StrictText)
+instance Conv LazyText
+instance Conv (Maybe LazyText)
+instance Conv Int
+instance Conv (Maybe Int)
+instance Conv Int64
+instance Conv (Maybe Int64)
+instance Conv Double
+instance Conv (Maybe Double)
+instance Conv Bool
+instance Conv (Maybe Bool)
+instance Conv UUID
+instance Conv (Maybe UUID)
+instance Conv Day
+instance Conv (Maybe Day)
+instance Conv UTCTime
+instance Conv (Maybe UTCTime)
+instance Conv LocalTime
+instance Conv (Maybe LocalTime)
+instance Conv TimeOfDay
+instance Conv (Maybe TimeOfDay)
+
+instance Conv a => Conv (CI a)
+
 instance Conv a => Conv [a] where
   type OpaRep [a] = [OpaRep a]
   conv = fmap conv
+
+instance (Conv a, Conv b) => Conv (a, b) where
+  type OpaRep (a, b) = (OpaRep a, OpaRep b)
+  conv (a, b) = (conv a, conv b)
+
+instance (Conv a, Conv b, Conv c) => Conv (a, b, c) where
+  type OpaRep (a, b, c) = (OpaRep a, OpaRep b, OpaRep c)
+  conv (a, b, c) = (conv a, conv b, conv c)
+
+instance (Conv a, Conv b, Conv c, Conv d) => Conv (a, b, c, d) where
+  type OpaRep (a, b, c, d) = (OpaRep a, OpaRep b, OpaRep c, OpaRep d)
+  conv (a, b, c, d) = (conv a, conv b, conv c, conv d)
+
+instance (Conv a, Conv b, Conv c, Conv d, Conv e) => Conv (a, b, c, d, e) where
+  type OpaRep (a, b, c, d, e) = (OpaRep a, OpaRep b, OpaRep c, OpaRep d, OpaRep e)
+  conv (a, b, c, d, e) = (conv a, conv b, conv c, conv d, conv e)
+
+instance (Conv a, Conv b, Conv c, Conv d, Conv e, Conv f) => Conv (a, b, c, d, e, f) where
+  type OpaRep (a, b, c, d, e, f) = (OpaRep a, OpaRep b, OpaRep c, OpaRep d, OpaRep e, OpaRep f)
+  conv (a, b, c, d, e, f) = (conv a, conv b, conv c, conv d, conv e, conv f)
+
+instance (Conv a, Conv b, Conv c, Conv d, Conv e, Conv f, Conv g) => Conv (a, b, c, d, e, f, g) where
+  type OpaRep (a, b, c, d, e, f, g) = (OpaRep a, OpaRep b, OpaRep c, OpaRep d, OpaRep e, OpaRep f, OpaRep g)
+  conv (a, b, c, d, e, f, g) = (conv a, conv b, conv c, conv d, conv e, conv f, conv g)
+
+instance (Conv a, Conv b, Conv c, Conv d, Conv e, Conv f, Conv g, Conv h) => Conv (a, b, c, d, e, f, g, h) where
+  type OpaRep (a, b, c, d, e, f, g, h) = (OpaRep a, OpaRep b, OpaRep c, OpaRep d, OpaRep e, OpaRep f, OpaRep g, OpaRep h)
+  conv (a, b, c, d, e, f, g, h) = (conv a, conv b, conv c, conv d, conv e, conv f, conv g, conv h)
