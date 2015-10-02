502c090518a1efd21a58cd1fa69b366962802dfc
Adam Bergmark <adam@bergmark.nl>
2015-10-02 17:03:10 +0200

    More documentation in the Main module

--- example/Main.hs
+++ example/Main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE
     GeneralizedNewtypeDeriving
+  , InstanceSigs
+  , NoMonomorphismRestriction
   , ScopedTypeVariables
   #-}
 module Main (main) where
@@ -7,6 +9,7 @@ module Main (main) where
 import Prelude.Compat
 
 import Control.Monad.Reader
+import Data.UUID
 import qualified Data.UUID.V4 as UUID (nextRandom)
 
 import Silk.Opaleye
@@ -20,23 +23,52 @@ main = do
   -- You can create a config by only providing 'ConnectInfo' to
   -- 'defaultConfig' which will generate a default connection pool. If
   -- you want to configure the pool use 'makeConfig'.
-  cfg <- defaultConfig $ ConnectInfo
+  --
+  -- The default setting type is 'Config_' = 'Config ()', See
+  -- 'Girella.Config' for more info about the type argument.
+  cfg :: Config_
+      <- defaultConfig $ ConnectInfo
            { connectHost     = "localhost"
            , connectPort     = 5432
            , connectUser     = "test"
            , connectPassword = "1234"
            , connectDatabase = "test"
            }
-  flip runReaderT cfg $ unTransStack queries
 
-queries :: MyTransStack ()
-queries = do
+  -- Create your own type or use a 'ReaderT' to store the 'Config'.
+  runMyTransStack cfg doThings
+  runInReaderT    cfg doThings
+
+-- | Normal runner for a transformer.
+-- 'Config_' = 'Config ()'. See 'Girella.Config' for customization.
+runMyTransStack :: Config_ -> MyTransStack a -> IO a
+runMyTransStack cfg = flip runReaderT cfg . unTransStack
+
+-- | Runs our queries in just a 'ReaderT' instead.
+runInReaderT :: Config c -> ReaderT (Config c) IO a -> IO a
+runInReaderT cfg = flip runReaderT cfg
+
+-- | 'MonadPool' denotes an environment that can run transactions of
+-- queries. Transactions in MonadPool's can't be combined, they will
+-- run separately.
+doThings :: (MonadPool m, MonadIO m) => m ()
+doThings = do
   i <- liftIO UUID.nextRandom
-  people :: [Example.PersonH] <- runTransaction $ do
-    Example.insert i "Aaron Aardvark" 12 (Just Example.Male)
-    runQuery Example.allByName
+  people <- runTransaction $ myTransaction i
   liftIO $ print people
 
+-- | A 'Transaction' form just that, a database Transaction. Note that
+-- we don't run the transaction here, which is why you can combine
+-- multiple transactions into one.
+--
+-- By combining these queries here we prevent them from running in isolation of each other.
+myTransaction :: Transaction m => UUID -> m [Example.PersonH]
+myTransaction i = do
+  Example.insert i "Aaron Aardvark" 12 (Just Example.Male)
+  runQuery Example.allByName
+
+-- | The Transformer stack, we need to stuff a Config somewhere in
+-- there to run queries.
 newtype MyTransStack a = MyTransStack { unTransStack :: ReaderT Config_ IO a }
   deriving
     ( Applicative
@@ -46,5 +78,12 @@ newtype MyTransStack a = MyTransStack { unTransStack :: ReaderT Config_ IO a }
     , MonadReader Config_
     )
 
+-- | Defines how to run transactions in our stack, typically by using
+-- 'defaultRunTransaction' which needs a way to get the 'Config' out
+-- of our stack.
 instance MonadPool MyTransStack where
-  runTransaction = defaultRunTransaction ask
+  runTransaction :: Q a -> MyTransStack a
+  runTransaction = defaultRunTransaction getConfig
+    where
+      getConfig :: MyTransStack Config_
+      getConfig = ask
