dcbc8b591c91750165e1c7c7db3eff58eddc5cbd
Erik Hesselink <hesselink@gmail.com>
2016-01-11 16:40:19 +0100

    Change aggregators to return Nullables.
    Also adds *Grouped aggregators that are only meant to use in
    combination with `groupBy` where you know they can't return Nullables.
    See https://github.com/tomjaguarpaw/haskell-opaleye/issues/138

--- src/Silk/Opaleye/Aggregation.hs
+++ src/Silk/Opaleye/Aggregation.hs
@@ -4,14 +4,20 @@
   #-}
 module Silk.Opaleye.Aggregation
   ( sum_
+  , sumGrouped
   , count
   , avg
+  , avgGrouped
   , max_
+  , maxGrouped
   , maxNullable
   , min_
+  , minGrouped
   , minNullable
   , boolOr
+  , boolOrGrouped
   , boolAnd
+  , boolAndGrouped
 --  , stringAgg
   , safeCoerceAgg
   -- * Renames
@@ -25,7 +31,7 @@ import Data.Int (Int64)
 import Data.Profunctor (Profunctor, dimap, rmap)
 
 import Opaleye.Aggregate (Aggregator, aggregate)
-import Opaleye.Column (Column, Nullable)
+import Opaleye.Column (Column, Nullable, toNullable)
 import Opaleye.PGTypes (PGBool)
 import qualified Opaleye.Aggregate as A (avg, boolAnd, boolOr, count, groupBy, max, min, sum)
 
@@ -37,34 +43,52 @@ groupBy_ = A.groupBy
 
 -- TODO Constrain to numeric types.
 -- http://www.postgresql.org/docs/9.4/static/functions-aggregate.html
-sum_ :: ShowConstant a => Aggregator (Column a) (Column a)
-sum_ = safeCoerceAgg A.sum
+sumGrouped :: ShowConstant a => Aggregator (Column a) (Column a)
+sumGrouped = safeCoerceAgg A.sum
+
+sum_ :: ShowConstant a => Aggregator (Column a) (Column (Nullable a))
+sum_ = toNullable <$> sumGrouped
 
 count :: ShowConstant a => Aggregator (Column a) (Column Int64)
 count = rmap safeCoerceFromRep A.count
 
 -- TODO Expand to numeric input (even integral)
 -- http://www.postgresql.org/docs/9.4/static/functions-aggregate.html
-avg :: Aggregator (Column Double) (Column Double)
-avg = safeCoerceAgg A.avg
+avgGrouped :: Aggregator (Column Double) (Column Double)
+avgGrouped = safeCoerceAgg A.avg
 
-max_ :: PGOrd (PGRep a) => Aggregator (Column a) (Column a)
-max_ = safeCoerceAgg A.max
+avg :: Aggregator (Column Double) (Column (Nullable Double))
+avg = toNullable <$> avgGrouped
+
+maxGrouped :: PGOrd (PGRep a) => Aggregator (Column a) (Column a)
+maxGrouped = safeCoerceAgg A.max
+
+max_ :: PGOrd (PGRep a) => Aggregator (Column a) (Column (Nullable a))
+max_ = toNullable <$> maxGrouped
 
 maxNullable :: PGOrd (PGRep a) => Aggregator (Column (Nullable a)) (Column (Nullable a))
 maxNullable = A.max
 
-min_ :: PGOrd (PGRep a) => Aggregator (Column a) (Column a)
-min_ = safeCoerceAgg A.min
+minGrouped :: PGOrd (PGRep a) => Aggregator (Column a) (Column a)
+minGrouped = safeCoerceAgg A.min
+
+min_ :: PGOrd (PGRep a) => Aggregator (Column a) (Column (Nullable a))
+min_ = toNullable <$> minGrouped
 
 minNullable :: PGOrd (PGRep a) => Aggregator (Column (Nullable a)) (Column (Nullable a))
 minNullable = A.min
 
-boolOr :: PGRep a ~ PGBool => Aggregator (Column a) (Column a)
-boolOr = safeCoerceAgg A.boolOr
+boolOrGrouped :: PGRep a ~ PGBool => Aggregator (Column a) (Column a)
+boolOrGrouped = safeCoerceAgg A.boolOr
 
-boolAnd :: PGRep a ~ PGBool => Aggregator (Column a) (Column a)
-boolAnd = safeCoerceAgg A.boolAnd
+boolOr :: PGRep a ~ PGBool => Aggregator (Column a) (Column (Nullable a))
+boolOr = toNullable <$> boolOrGrouped
+
+boolAndGrouped :: PGRep a ~ PGBool => Aggregator (Column a) (Column a)
+boolAndGrouped = safeCoerceAgg A.boolAnd
+
+boolAnd :: PGRep a ~ PGBool => Aggregator (Column a) (Column (Nullable a))
+boolAnd = toNullable <$> boolAndGrouped
 
 -- TODO Needs opaleye 0.4
 --stringAgg :: PGRep a ~ PGText => Column a -> Aggregator (Column a) (Column a)
