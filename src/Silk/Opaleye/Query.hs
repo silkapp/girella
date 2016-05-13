69ca7b81f701fe19e01af644618893641a4409e0
Erik Hesselink <hesselink@gmail.com>
2016-05-13 17:11:34 +0200

    Add folds to consume query results.
    This uses a cursor to incrementally stream the results.

--- src/Silk/Opaleye/Query.hs
+++ src/Silk/Opaleye/Query.hs
@@ -15,6 +15,10 @@ module Silk.Opaleye.Query
   , runQuery
   , runQueryFirst
   , runQueryString
+  , foldQueryInternalExplicit
+  , foldQueryInternal
+  , foldQueryExplicit
+  , foldQuery
   ) where
 
 import Control.Monad.Reader
@@ -32,7 +36,7 @@ import Opaleye.QueryArr
 import Opaleye.RunQuery (QueryRunner)
 import Opaleye.Table
 import qualified Opaleye.Manipulation as M (runDelete, runInsert, runInsertReturning, runUpdate)
-import qualified Opaleye.RunQuery     as M (runQueryExplicit)
+import qualified Opaleye.RunQuery     as M (runQueryExplicit, foldQueryExplicit)
 
 import Silk.Opaleye.Conv
 import Silk.Opaleye.ShowConstant
@@ -87,7 +91,59 @@ runQueryInternalExplicit
 runQueryInternalExplicit qr q = liftQ $ do
   conn <- ask
   unsafeIOToTransaction . M.runQueryExplicit qr conn $ label (ppCallStack ?loc) q
-  where ppCallStack = maybe "no call stack available" (showSrcLoc . snd) . lastMay . getCallStack
+
+foldQueryInternalExplicit
+  :: ( ?loc :: CallStack
+     , Transaction m
+     )
+  => QueryRunner columns haskells
+  -> Query columns
+  -> a
+  -> (a -> haskells -> IO a)
+  -> m a
+foldQueryInternalExplicit qr q e f = liftQ $ do
+  conn <- ask
+  unsafeIOToTransaction $ M.foldQueryExplicit qr conn (label (ppCallStack ?loc) q) e f
+
+foldQueryInternal
+  :: ( ?loc :: CallStack
+     , Default QueryRunner columns haskells
+     , Transaction m
+     )
+  => Query columns
+  -> a
+  -> (a -> haskells -> IO a)
+  -> m a
+foldQueryInternal = foldQueryInternalExplicit def
+
+foldQueryExplicit
+  :: ( ?loc :: CallStack
+     , haskells ~ OpaRep domain
+     , Conv domain
+     , Transaction m
+     )
+  => QueryRunner columns haskells
+  -> Query columns
+  -> a
+  -> (a -> domain -> IO a)
+  -> m a
+foldQueryExplicit qr q e f = foldQueryInternalExplicit qr q e (\x -> f x . conv)
+
+foldQuery
+  :: ( ?loc :: CallStack
+     , Default QueryRunner columns haskells
+     , haskells ~ OpaRep domain
+     , Conv domain
+     , Transaction m
+     )
+  => Query columns
+  -> a
+  -> (a -> domain -> IO a)
+  -> m a
+foldQuery = foldQueryExplicit def
+
+ppCallStack :: CallStack -> String
+ppCallStack = maybe "no call stack available" (showSrcLoc . snd) . lastMay . getCallStack
 
 -- | Opaleye's runQuery inside a Transaction, does not use 'Conv'
 runQueryInternal
